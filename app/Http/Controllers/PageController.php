23e5b1e255c0cfe5db7a923e7f6015035e43d2c9
battistaennio <ennio.battista99@gmail.com>
2024-09-11 15:36:09 +0200

    feat: routes managed by PageController

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -4,7 +4,23 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Models\Movie;
+
 class PageController extends Controller
 {
-    //
+    public function index()
+    {
+
+        return view('home');
+    }
+
+    public function contacts()
+    {
+        return view('contacts');
+    }
+
+    public function about()
+    {
+        return view('about');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PageController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,14 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('home');
-})->name('home');
+Route::get('/', [PageController::class, 'index'])->name('home');
 
-Route::get('/chi-siamo', function () {
-    return view('about');
-})->name('about');
+Route::get('/contatti', [PageController::class, 'contacts'])->name('contacts');
 
-Route::get('/contatti', function () {
-    return view('contacts');
-})->name('contacts');
+Route::get('/chi-siamo', [PageController::class, 'about'])->name('about');
