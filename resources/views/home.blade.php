f02d5b7a45f157f32640efb08e44837a19169164
battistaennio <ennio.battista99@gmail.com>
2024-09-11 16:11:07 +0200

    style: add flex centre to cards row

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,10 +9,10 @@
     <div class="container my-5">
         <h1>{{ $title }}</h1>
 
-        <div class="row">
+        <div class="row justify-content-center">
 
             @foreach ($movies as $movie)
-                <div class="card text-center m-3" style="width: 18rem;">
+                <div class="card text-center m-2" style="width: 18rem;">
                     <div class="card-body">
                         <h5 class="card-title">{{ $movie->title }}</h5>
                         <h6 class="card-subtitle mb-2 text-body-secondary">{{ $movie->original_title }}</h6>
